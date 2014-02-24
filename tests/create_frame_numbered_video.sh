e75c348f906da7283408430de5dddea24057f7b0
Sudip Regmi <sudip_regmi@yahoo.com>
2014-02-24 06:04:40 +0000

    Create a video for testing

---
+++ tests/create_frame_numbered_video.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm /tmp/VideoFrames/*
+mkdir -p /tmp/VideoFrames/
+for i in `seq 1 9000`; do
+    convert -size 800x600 -background lightblue -fill blue -font Courier -pointsize 200 label:$i /tmp/VideoFrames/img$i.jpg
+done
+cd /tmp/VideoFrames/
+ffmpeg -f image2 -i /tmp/VideoFrames/img%d.jpg -b 800k /tmp/VideoFrames/out.mp4
+ffmpeg -f image2 -i /tmp/VideoFrames/img%d.jpg -b 800k /tmp/VideoFrames/out.mpg
