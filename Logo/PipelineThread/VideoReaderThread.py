c833da9d8185d3fba56ecd660d71e53f9ee68283
Sudip Regmi <sudip@zigvu.com>
2014-09-28 11:36:39 -0500

    Separate VideoReaderThread

---
+++ Logo/PipelineThread/VideoReaderThread.py
@@ -0,0 +1,157 @@
+import os, time
+import multiprocessing
+from multiprocessing import JoinableQueue, Process, Manager
+from threading import Thread
+from collections import OrderedDict
+import json
+import logging
+
+import VideoReader
+
+from Logo.PipelineMath.Rectangle import Rectangle
+from Logo.PipelineMath.BoundingBoxes import BoundingBoxes
+
+from Logo.PipelineCore.ConfigReader import ConfigReader
+from Logo.PipelineCore.JSONReaderWriter import JSONReaderWriter
+from Logo.PipelineCore.CaffeNet import CaffeNet
+
+from Logo.PipelineThread.PostProcessThread import PostProcessThread
+from Logo.PipelineThread.PostProcessThread import framePostProcessorRun
+
+class VideoReaderThread( Thread ):
+  """ Class for extracting frames from video """
+  def __init__( self, leveldbQueue, leveldbFolder, jsonFolder, videoFileName, configReader, frameStartNum ):
+    super(VideoReaderThread, self).__init__()
+    self.leveldbQueue = leveldbQueue
+    self.leveldbFolder = leveldbFolder
+    self.jsonFolder = jsonFolder
+    self.videoFileName = videoFileName
+    self.configReader = configReader
+    self.scales = self.configReader.sw_scales
+    self.frameStep = self.configReader.sw_frame_density
+    self.videoFrameReader = VideoReader.VideoFrameReader(40, 40, self.videoFileName)
+    self.videoFrameReader.generateFrames()
+    self.videoFrameReader.startLogger()
+
+    # Load video - since no expilicit synchronization exists to check if
+    # VideoReader is ready, wait for 10 seconds
+    time.sleep(10)
+
+    # Get frame dimensions and create bounding boxes
+    self.frame = self.videoFrameReader.getFrameWithFrameNumber(1)
+    while not self.frame:
+      self.frame = self.videoFrameReader.getFrameWithFrameNumber(1)
+    self.imageDim = Rectangle.rectangle_from_dimensions(self.frame.width, self.frame.height)
+    self.patchDimension = Rectangle.rectangle_from_dimensions(\
+      self.configReader.sw_patchWidth, self.configReader.sw_patchHeight)
+    self.staticBoundingBoxes = BoundingBoxes(self.imageDim, \
+      self.configReader.sw_xStride, self.configReader.sw_xStride, self.patchDimension)
+
+    self.fps = self.videoFrameReader.fps
+    self.lengthInMicroSeconds = self.videoFrameReader.lengthInMicroSeconds
+    self.totalNumOfFrames = int(self.fps * self.lengthInMicroSeconds / 1000000.0)
+
+    #self.startFrameNumber = self.configReader.ci_videoFrameNumberStart
+    self.startFrameNumber = frameStartNum
+    self.numFramesPerLeveldb = self.configReader.ci_numFramesPerLeveldb
+    self.leveldbFolderSize = self.configReader.ci_maxLeveldbSizeMB
+
+    # Video name prefix for all frames/patches:
+    self.videoId = os.path.basename(videoFileName).split('.')[0]
+ 
+  def run( self ):
+    """ Spawn as many processes as there are GPUs"""
+    p = Process( target=startVideoReaderProcess, args={self} )
+    p.start()
+
+def startVideoReaderProcess( self ):
+  """ Run the VideoReader Thread """
+  # Initialize variables
+  currentFrameNum = self.startFrameNumber # frame number being extracted
+  extractedFrameCounter = 0               # total number of extracted frames
+  curLeveldbFolder = None                 # folder where to write leveldb
+  videoLeveldb = None                     # levedb object from VideoReader
+  leveldbMapping = None                   # mapping between patches in leveldb and corresponding jsons
+  leveldbId = 0                           # number of leveldb created
+
+  videoFrameReader = VideoReader.VideoFrameReader(40, 40, self.videoFileName)
+  videoFrameReader.generateFrames()
+
+  # Load video - since no expilicit synchronization exists to check if
+  # VideoReader is ready, wait for 10 seconds
+  time.sleep(10)
+  
+  # Main loop to go through video
+  logging.info("Start patch extraction")
+  while (not videoFrameReader.eof) or (currentFrameNum <= videoFrameReader.totalFrames):
+    # Create new leveldbs for each set of numFramesPerLeveldb frames
+    if ((extractedFrameCounter %  self.numFramesPerLeveldb) == 0):
+      # If ready, save leveldb and put in queue for CaffeNet
+      if videoLeveldb != None:
+        logging.info("Saving leveldb ID: %d" % (leveldbId))
+        videoLeveldb.saveLevelDb()
+        with open(leveldbMappingFile, "w") as f :
+          json.dump(leveldbMapping, f, indent=2)
+        # sleep some time so that file handles get cleared
+        time.sleep(5)
+        self.leveldbQueue.put(curLeveldbFolder)
+      # If leveldb folder is full, wait until dump
+      if self.leveldbFolderSize > 0:
+        leveldbFolderSize = ConfigReader.dir_size(self.leveldbFolder)
+        while leveldbFolderSize >= self.leveldbFolderSize:
+          logging.info("Waiting for leveldb folder to empty")
+          time.sleep(5)
+          leveldbFolderSize = ConfigReader.dir_size(self.leveldbFolder)
+      # Set up new levedb
+      extractedFrameCounter = 0
+      leveldbPatchCounter = 0
+      curLeveldbFolder = os.path.join(self.leveldbFolder, "%s_leveldb_%d" % (self.videoId, leveldbId))
+      leveldbMappingFile = os.path.join(curLeveldbFolder, "leveldb_mapping.json")
+      videoLeveldb = VideoReader.VideoLevelDb(curLeveldbFolder)
+      videoLeveldb.setVideoFrameReader(videoFrameReader)
+      leveldbMapping = OrderedDict()
+      leveldbId += 1
+      logging.info("%d percent video processed" % (int(100.0 * currentFrameNum/self.totalNumOfFrames)))
+    # Start json annotation file
+    jsonName = os.path.join(self.jsonFolder, "%s_frame_%s.json" % (self.videoId, currentFrameNum))
+    jsonAnnotation = JSONReaderWriter(jsonName, create_new=True)
+    jsonAnnotation.initializeJSON(self.videoId, currentFrameNum, self.imageDim, self.scales)
+    # Put patch into leveldb
+    for scale in self.scales:
+      patchNum = 0
+      for box in self.staticBoundingBoxes.getBoundingBoxes(scale):
+        # Generate leveldb patch and add to json
+        leveldbPatchCounter = videoLeveldb.savePatch(currentFrameNum, scale, \
+          box[0], box[1], box[2], box [3])
+        jsonAnnotation.addPatch(scale, patchNum, leveldbPatchCounter, \
+          box[0], box[1], box[2], box [3])
+        leveldbMapping[leveldbPatchCounter] = jsonName
+        # Increment counters
+        patchNum += 1
+    # Save annotation file
+    jsonAnnotation.saveState()
+    logging.debug("Finished working on frame %d" % currentFrameNum)
+    currentFrameNum += self.frameStep
+    extractedFrameCounter += 1
+  # end while
+
+  # For the last leveldb group, save and put in queue
+  if videoLeveldb != None:
+    logging.info("Saving leveldb ID: %d" % (leveldbId))
+    videoLeveldb.saveLevelDb()
+    with open(leveldbMappingFile, "w") as f :
+      json.dump(leveldbMapping, f, indent=2)
+    self.leveldbQueue.put(curLeveldbFolder)
+
+  # HACK: work around so that VideoLevelDb releases lock on curLeveldbFolder
+  videoLeveldb = None
+  # HACK: quit video reader gracefully
+  currentFrameNum = videoFrameReader.totalFrames
+  while not videoFrameReader.eof or currentFrameNum <= videoFrameReader.totalFrames:
+    videoFrameReader.seekToFrameWithFrameNumber(currentFrameNum)
+    currentFrameNum += 1
+
+  # Put poison pills and wait to join all threads
+  logging.info("Done with all patch extraction. Waiting for caffe thread to join")
+  self.leveldbQueue.put(None)
+  self.leveldbQueue.join()
