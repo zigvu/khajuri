1f79e252a09e43b7d99b1efce91c08b98b103ee6
Sudip Regmi <sudip@zigvu.com>
2015-04-18 02:43:10 -0500

    Deleted old files

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def readme():
     with open('README.md') as f:
         return f.read()
 
-setup(name='VideoPipeline',
+setup(name='khajuri',
       version='0.1',
-      description='VideoPipeline',
+      description='khajuri',
       long_description=readme(),
       classifiers=[
         'Copyright (C) ZigVu',
       ],
-      packages=['Controller', 'Logo', 'tests', 'plugins', 'Swf', 'VideoReader' ],
+      packages=find_packages(),
       test_suite='nose.collector',
       tests_require=['nose', 'nose-cover3'],
       include_package_data=True,
-      zip_safe=False)
+      zip_safe=False,
+      entry_points={
+        'console_scripts': [
+            'postprocess=tool.pp:main',
+            'chia=tool.pp:main',
+        ],
+      )
