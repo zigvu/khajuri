f7e58aab0ac075ecd25992b708f9dd36e8bc133c
Sudip Regmi <sudip@zigvu.com>
2015-06-16 00:27:00 -0500

    Save Testing a single frame progress

--- postprocessing/type/Rect.py
+++ postprocessing/type/Rect.py
@@ -16,3 +16,19 @@ class Rect( object ):
     distanceSquaredSum = ( ( self.center[ 0 ] - other.center[ 0 ] ) ** 2 ) + \
         ( ( self.center[ 1 ] - other.center[ 1 ] )  ** 2 )
     return math.sqrt( distanceSquaredSum )
+
+  def intersect( self, other ):
+    RectAX1 = self.x
+    RectAX2 = self.x + self.w
+    RectAY1 = self.y
+    RectAY2 = self.y + self.h
+
+    RectBX1 = other.x
+    RectBX2 = other.x + other.w
+    RectBY1 = other.y
+    RectBY2 = other.y + other.h
+
+    if (RectAX1 < RectBX2 and RectAX2 > RectBX1 and RectAY1 < RectBY2 and RectAY2 > RectBY1):
+      return True
+
+    return False
