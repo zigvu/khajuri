3976edb0d5f1134040012ffe358fb1ce5cf0ba31
eacharya <eacharya@gmail.com>
2015-09-28 22:06:41 -0700

    Issue 117: Remove best inferred scale logic

--- Logo/PipelineMath/FramePostProcessor.py
+++ Logo/PipelineMath/FramePostProcessor.py
@@ -1,4 +1,3 @@
-from Logo.PipelineMath.ScaleSpaceCombiner import ScaleSpaceCombiner
 from Logo.PipelineMath.PeaksExtractor import PeaksExtractor
 
 from postprocessing.type.Localization import Localization
@@ -12,27 +11,33 @@ class FramePostProcessor(object):
     """Initialize values"""
     self.classId = classId
     self.config = config
+    self.slidingWindowCfg = self.config.slidingWindow
     self.logger = self.config.logging.logger
     self.frame = frame
     self.zDistThreshold = zDistThreshold
 
   def localize(self):
     """Collect and analyze detection results"""
-    scaleSpaceCombiner = ScaleSpaceCombiner(
-        self.classId, self.config, self.frame, self.zDistThreshold)
-    lclzOrigScale, lclzPixelMap = scaleSpaceCombiner.getBestInferredPixelMap()
-    lclzPixelMap.setScale(1.0)
-    lclzPeaks = PeaksExtractor(lclzPixelMap, self.config)
-    lclzPatches = lclzPeaks.getPeakBboxes()
-    for lp in lclzPatches:
-      # self.logger.debug(
-      #     'Localization at: %s, with intensity: %s for class %s and zDist %s' %
-      #     (lp['bbox'].json_format(), lp['intensity'], self.classId,
-      #      self.zDistThreshold))
-      rect = Rect(
-          lp['bbox'].x0, lp['bbox'].y0, lp['bbox'].width, lp['bbox'].height)
-      l = Localization(
-          self.zDistThreshold, self.classId, rect, lp['intensity'],
-          lclzOrigScale)
-      self.frame.addLocalization(self.classId, l)
-    return lclzPatches
+    # set up decay pixel map
+    pixelMapper = PixelMapper(
+      self.classId, self.config, self.frame, self.zDistThreshold)
+    pixelMapper.setupScaleDecayedMapCache(
+        self.slidingWindowCfg.sw_scale_decay_factors)
+    # get localizations for all scales
+    for lclzOrigScale in self.slidingWindowCfg.sw_scales:
+      lclzPixelMap = pixelMapper.getScaleDecayedMap(lclzOrigScale)
+      lclzPixelMap.setScale(1.0)
+      lclzPeaks = PeaksExtractor(lclzPixelMap, self.config)
+      lclzPatches = lclzPeaks.getPeakBboxes()
+      for lp in lclzPatches:
+        # self.logger.debug(
+        #     'Localization at: %s, with intensity: %s for class %s and zDist %s' %
+        #     (lp['bbox'].json_format(), lp['intensity'], self.classId,
+        #      self.zDistThreshold))
+        rect = Rect(
+            lp['bbox'].x0, lp['bbox'].y0, lp['bbox'].width, lp['bbox'].height)
+        loc = Localization(
+            self.zDistThreshold, self.classId, rect, lp['intensity'],
+            lclzOrigScale)
+        self.frame.addLocalization(self.classId, loc)
+      return lclzPatches
